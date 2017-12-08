2bd1b71da9b36569020d2dc081c719079f29ed3f
Siddharth Thakurdesai <siddharthtd@gmail.com>
2017-12-08 09:41:41 -0800

    Created a function for Filtering Deauthentication Packets.

--- ddos_deauth_detector.py
+++ ddos_deauth_detector.py
@@ -7,8 +7,23 @@ import scapy
 class DeauthenticationDetector:
 
     def __init__(self, *args, **kwargs):
-        
+
         self.args = args
         self.kwargs = kwargs
         self.data={}
         self.Sniffing_Start()
+
+
+    def extract_packets(self, pkt):
+
+        if pkt.haslayer(scapy.Dot11Deauth):
+            victim1 = pkt.addr2
+            victim2 = pkt.addr1
+            if str([victim1, victim2]) in self.data.keys():
+                self.data[str([victim1, victim2])] = self.data[str([victim1, victim2])] + 1
+            else:
+                self.data[str([victim1, victim2])] = 1
+            self.print_values()
+    return
+
+    
