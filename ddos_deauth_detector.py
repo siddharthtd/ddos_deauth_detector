52723d95df21993048e54f380b479bea4c83d655
Siddharth Thakurdesai <siddharthtd@gmail.com>
2017-12-09 12:52:12 -0800

    running program, exception handling to be implemented for socket errors.

--- ddos_deauth_detector.py
+++ ddos_deauth_detector.py
@@ -1,24 +1,23 @@
-Field={'count': (5,False), 'iface': (None, True), 'timeout': (None, False)}
+Field = {'count': (5, False), 'iface': (None, True), 'timeout': (None, False)}
 
 import sys
-import scapy
+import scapy.all as scapy
 
 
-class DeauthenticationDetector:
+class Deauth_det:
 
     def __init__(self, *args, **kwargs):
 
         self.args = args
         self.kwargs = kwargs
-        self.data={}
+        self.data = {}
         self.Sniffing_Start()
-
 
     def extract_packets(self, pkt):
 
         if pkt.haslayer(scapy.Dot11Deauth):
-            victim1 = pkt.addr2
-            victim2 = pkt.addr1
+            victim1 = pkt.addr1
+            victim2 = pkt.addr2
             if str([victim1, victim2]) in self.data.keys():
                 self.data[str([victim1, victim2])] = self.data[str([victim1, victim2])] + 1
             else:
@@ -27,17 +26,28 @@ class DeauthenticationDetector:
         return
 
     def print_values(self):
-        line = 0
-        for a, b in self.data.iteritems():
+        count = 0
+        for a,b in self.data.iteritems():
             v1, v2 = eval(a)
-            print "\t[#] Deauthentication Packet : {} <---> {} | Packets : {}".format(v1, v2, b)
-            line += 1
+            print("\nDe-authentication Packet:{}<--->{}\nPackets:{}".format(v1, v2, b))
+            count += 1
 
         # Backspace Trick
         sys.stdout.write("\033[{}A".format(line))
         return
 
-
     def Sniffing_Start(self):
-        scapy.sniff(prn = self.extract_packets, *self.args, **self.kwargs)
+        scapy.sniff(prn=self.extract_packets, *self.args, **self.kwargs)
         return
+
+
+def main(*args, **kwargs):
+    Deauth_det(*args, **kwargs)
+    return
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        main(iface=sys.argv[1])
+    else:
+        print("[Error]Please Provide Monitor Mode Interface Name ALso \n\n\t:~# sudo {} mon0".format(sys.argv[0]))
